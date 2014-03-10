86a76225396447a8a1d4ca57a8028e99c96eded2
Disa Mhembere <dmhembe1@jhu.edu>
2014-03-10 12:28:48 -0400

    Added metadata mouse_retina

--- CASC/src/mouse_retina.R
+++ CASC/src/mouse_retina.R
@@ -1,10 +1,5 @@
-# mouse_retina.R
-# Created by Disa Mhembere on 2014-03-10.
-# Email: disa@jhu.edu
-# Copyright (c) 2014. All rights reserved.
-
-# Create: mouse_retina_1.graphml
-
+# Mouse
+# mouse_retina_1.graphml
 require(igraph)
 
 load("../RData/mouseretina.RData")
@@ -15,6 +10,11 @@ mcts <- merge(m.cells.types, mouse_somapositions[,2:5], by=intersect("cell_id", 
 mcts <- mcts[c("cell_id", "designation", "volgyi_type", "macneil_type", "coarse", "certainty", "x", "y", "z")]
 
 g <- graph.data.frame(mouse_contacts, directed=TRUE, vertices=mcts) 
+g$sensor <- "Electron Microscopy"
+g$source <- "Automatic discovery of cell types and microcircuitry from neural connectomics. Eric Jonas and Konrad Kording, submitted"
+g$genus <- "mouse"
+g$region <- "retina"
+
 write.graph(g, "graphs/mouse_retina_1.graphml", format="graphml")
 
 
